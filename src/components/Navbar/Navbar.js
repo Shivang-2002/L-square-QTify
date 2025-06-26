24baf3c997739dcd88e28c7633f4fc21016c65b0
Shivang Vidyadharan <shivangvidyadharan@Mac.domain.name>
2025-06-27 03:54:08 +0530

    Navbar and hero done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ function Navbar({ searchData }) {
         <Logo />
       </Link>
       <Search
-        placeholder="Search a song of your choice"
+        placeholder="Search"
         searchData={searchData}
       />
       <Button>Give Feedback</Button>
